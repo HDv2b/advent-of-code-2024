f93035aa499916f7439e473a824cc236a8a98763
Hussein Duvigneau <hussein@notmybase.com>
2024-12-01 18:28:00 +0100

    day 1

--- day-1/a.ts
+++ day-1/a.ts
@@ -2,8 +2,24 @@ import { runSolution } from '../utils.ts';
 
 /** provide your solution as the return of this function */
 export async function day1a(data: string[]) {
-  console.log(data);
-  return 0;
+  let left: number[] = [];
+  let right: number[] = [];
+
+  for (const row of data) {
+    const [l, r] = row.split(/\s+/g);
+    left.push(parseInt(l, 10));
+    right.push(parseInt(r, 10));
+  }
+
+  left = left.toSorted()
+  right = right.toSorted()
+
+  let total = 0;
+  for (const n in left) {
+    total += Math.abs(left[n] - right[n])
+  }
+
+  return total;
 }
 
 await runSolution(day1a);
--- day-1/b.ts
+++ day-1/b.ts
@@ -2,8 +2,24 @@ import { runSolution } from '../utils.ts';
 
 /** provide your solution as the return of this function */
 export async function day1b(data: string[]) {
-  console.log(data);
-  return 0;
+  let left: number[] = [];
+  let right: number[] = [];
+
+  for (const row of data) {
+    const [l, r] = row.split(/\s+/g);
+    left.push(parseInt(l, 10));
+    right.push(parseInt(r, 10));
+  }
+
+  left = left.toSorted()
+  right = right.toSorted()
+
+  let total = 0;
+  for (const l of left) {
+    total += l * right.filter(r => r === l).length
+  }
+
+  return total;
 }
 
 await runSolution(day1b);
